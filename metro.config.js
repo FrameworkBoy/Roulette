6ce6b30c704af9c216577cc3c8a161669ee188ff
andreltgribeiro <andreltgribeiro@gmail.com>
2026-04-23 19:25:34 -0300

    Remove native windows build

--- metro.config.js
+++ metro.config.js
@@ -1,20 +1,3 @@
 const { getDefaultConfig } = require('expo/metro-config');
-const path = require('path');
-const fs = require('fs');
 
-const rnwPath = fs.realpathSync(
-  path.resolve(require.resolve('react-native-windows/package.json'), '..'),
-);
-
-const config = getDefaultConfig(__dirname);
-
-config.resolver.blockList = [
-  new RegExp(
-    `${path.resolve(__dirname, 'windows').replace(/[/\\]/g, '/')}.*`,
-  ),
-  new RegExp(`${rnwPath}/build/.*`),
-  new RegExp(`${rnwPath}/target/.*`),
-  /.*\.ProjectImports\.zip/,
-];
-
-module.exports = config;
+module.exports = getDefaultConfig(__dirname);
